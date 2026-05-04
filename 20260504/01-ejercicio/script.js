188452be9d1b05896a90c26ae576b6a796dfb6f7
josemcnieto <josemcnieto@gmail.com>
2026-05-04 21:49:31 +0200

    primer JS

---
+++ 20260504/01-ejercicio/script.js
@@ -0,0 +1,6 @@
+console.log("! El cerebro de la web esta funcionando !");
+//Funcion saludar
+function saludar(){
+    let nombre = prompt("¿Como te llamas?");
+    alert("!Hola " + nombre +"! Ya eres programador/a.")
+}
