580550a8a7b310dc9c66e4083a133169652f1286
josemcnieto <josemcnieto@gmail.com>
2026-05-04 21:59:10 +0200

    segundo boton

--- 20260504/01-ejercicio/script.js
+++ 20260504/01-ejercicio/script.js
@@ -2,5 +2,19 @@ console.log("! El cerebro de la web esta funcionando !");
 //Funcion saludar
 function saludar(){
     let nombre = prompt("¿Como te llamas?");
-    alert("!Hola " + nombre +"! Ya eres programador/a.")
+    alert("!Hola " + nombre +"! Ya eres programador/a.");
 }
+// Funcion verificar acceso
+function verificarAcceso() {
+    let edad = prompt("Introduce tu edad :");
+
+    if (edad >=18) {
+        alert("!Acceso permitido");
+        console.log("Estado mayor de edad");
+    }
+        else{
+            alert("!Acceso denegado! Eres menor.");
+            console.log("Estado: Menor de edad detectado");
+        }
+    }
+
