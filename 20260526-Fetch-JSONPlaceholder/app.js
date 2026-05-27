392770d9887d8e34c0fe5fac65d0cff4289d360a
josemcnieto <josemcnieto@gmail.com>
2026-05-27 19:25:29 +0200

    ultimo enviado

--- 20260526-Fetch-JSONPlaceholder/app.js
+++ 20260526-Fetch-JSONPlaceholder/app.js
@@ -27,3 +27,55 @@ btnBuscar.addEventListener('click', async ()=> {
         pantalla.textContent = "❌ No se encontro la publicacion o hubo un error."
     }
 });
+
+// Control de accidentes (try .... catch)
+btnError.addEventListener('click', async ()=>{
+    pantalla.textContent = "⌛ Forzando un accidente...";
+
+    try{
+        //enviaamos al cartero a una ruta que no existe en el servidor
+        const data = await callAPI("/ruta-inventada-que-no-existe");
+        pantalla.textContent = JSON.stringify(data, null, 2);
+    }catch (error){
+        //la ruta da error 404 , el codigo salta directamente  aqui y no se rompoe la web
+         pantalla.textContent = `🛡 ¡ El escudo Try/Catch funciono! \nDetalle:${error.message}`;
+    
+}
+});
+
+//crear datos (post)
+//en las empresas, no solo leemos internet, tambien mandamos informacion(registros, compras...)
+
+fromCrear.addEventListener('submit',async (evento) => {
+    //1, Evitamos el parpadeo  de la web al enviar formulario
+    evento.preventDefault();
+
+    pantalla.textContent = "⌛ Empaquetando y enviando datos....";
+    //2. construimos  el "paquete " con la informacion de los inputs
+    const tituloNuevo = document.getElementById('inputTitulo').value;
+    const cuerpoNuevo = document.getElementById('inputCuerpo').value;
+
+    const paqueteDatos = {
+        title: tituloNuevo,
+        body: cuerpoNuevo,
+        userID: 1  //ponemos un ID de usuario fijo simulando que estamos logueados
+    };
+
+    try{
+        //3. llamamos al cartero , pero esta vez le pasamos "opciones" (metodo post y el body)
+        const respuestaServidor = await callAPI("/posts" , {
+            method: "POST", //metodo para crear
+            body: JSON.stringify(paqueteDatos) //convertimos nuestro objeto JS a texto JSON                       
+            });
+            //4, el servidor nos respone con el objeto creado (incluyendo su nuevo ID)
+            pantalla.textContent = ` ✔ ¡Creacion exitosa en el servidor!\n\n` +JSON.stringify(respuestaServidor,null,2)
+            //limpiamos los inputs
+            document.getElementById('inputTitulo').value = "";
+            document.getElementById('inputCuerpo').value = "";
+                           
+            }catch (error) {
+                pantalla.textContent = `❌ Fallo la creacion: ${error.message}`;
+            }
+        });
+    
+
