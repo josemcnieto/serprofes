50c6803eb2fa1b19cdd6b994d320ab44ab4b4509
josemcnieto <josemcnieto@gmail.com>
2026-06-18 20:47:29 +0200

    añadido el error

--- 20260616-api-backend/index.js
+++ 20260616-api-backend/index.js
@@ -21,6 +21,66 @@ let estudiantes = [
     {id: 2, nombre: "Jose", curso:"Node"}
 ];
 
+
+
+
+// 🚩ruta get : para leer datos
+//cuando alguien pregunte por "/api/estudiantes" , el servidor muestra la lista
+app.get("/api/estudiantes", (req,res)=>{
+    res.json(estudiantes);
+});
+
+
+//🚩 ruta  post: para guardar datos nuevos
+//cuando alguien envie informacion a "api/estudiantes", hacemos lo siguiente
+// app.post("/api/estudiantes", (req,res) =>{
+//     //A. atrapamos los datos que vienen de fuera (viven dentro de  req.body)
+//     const nuevoEstudiante = req.body;
+//     //B. metemos ese estudante nuevo en nuestra list usando .push()
+//     estudiantes.push(nuevoEstudiante);
+//     //C. le respondemos al usuario confirmando que toso ha ido bien
+//     res.json({
+//         mensaje: "¡Estudiante añadido con exito a la base de datos!",
+//         listaActualizada: estudiantes
+//     });
+// });
+
+
+app.post("/api/estudiantes", (req,res) =>{
+    //A. atrapamos los datos que vienen de f uera (viven dentro de  req.body)
+    //const nuevoEstudiante = req.body;
+    const {nombre, curso}=req.body;
+
+
+    //reto 3
+    if (!nombre || !curso || nombre.trim() ===" || curso.trim( )"){
+        return res.status(400).json({
+            error : "Error: El nombre  el curso son obligatorios."
+        })
+    }
+
+    //reto nivel 2: id automatico
+    const nuevoEstudiante = {
+        id: estudiantes.length+1,
+        nombre :nombre,
+        curso: curso
+
+        
+    };
+
+    //B. metemos ese estudante nuevo en nuestra list usando .push()
+    estudiantes.push(nuevoEstudiante);
+    //C. le respondemos al usuario confirmando que toso ha ido bien
+    res.json({
+        mensaje: "¡Estudiante añadido con exito a la base de datos!",
+        listaActualizada: estudiantes
+    });
+});
+
+
+
+
+// reto 1. base de datos profesores
 let profesores = [
     {id: 1, nombre: "Jorge", asignatura: "Desarrollo Web"},
     {id: 2, nombre: "Gonzalo", asignatura:"Machine Learnign"}
@@ -43,30 +103,6 @@ profesores.push(nuevoProfesor);
 
 
 
-
-
-
-// 🚩ruta get : para leer datos
-//cuando alguien pregunte por "/api/estudiantes" , el servidor muestra la lista
-app.get("/api/estudiantes", (req,res)=>{
-    res.json(estudiantes);
-});
-
-
-
-//🚩 ruta  post: para guardar datos nuevos
-//cuando alguien envie informacion a "api/estudiantes", hacemos lo siguiente
-app.post("/api/estudiantes", (req,res) =>{
-    //A. atrapamos los datos que vienen de fuera (viven dentro de  req.body)
-    const nuevoEstudiante = req.body;
-    //B. metemos ese estudante nuevo en nuestra list usando .push()
-    estudiantes.push(nuevoEstudiante);
-    //C. le respondemos al usuario confirmando que toso ha ido bien
-    res.json({
-        mensaje: "¡Estudiante añadido con exito a la base de datos!",
-        listaActualizada: estudiantes
-    });
-});
 
 
 //5. encendemos el motor 💨
